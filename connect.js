e698a0ab6b7f00dc913c82ffd7b887a53a0f30be
caesar <joaovictorcsatu@gmail.com>
2022-04-02 19:32:36 -0300

    Initiated Sequelize config

---
+++ connect.js
@@ -0,0 +1,21 @@
+const { Sequelize } = require("sequelize");
+require("dotenv").config();
+
+const sequelize = new Sequelize(
+  process.env.DB,
+  process.env.username,
+  process.env.password,
+  {
+    host: "localhost",
+    dialect: "postgres",
+  }
+);
+
+try {
+  await sequelize.authenticate();
+  console.log("Connected to DB");
+} catch (error) {
+  console.log(error);
+}
+
+module.exports = 
