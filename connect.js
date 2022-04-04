0288ec209abb8c6b217cd3f3d9618390094030ce
caesar <joaovictorcsatu@gmail.com>
2022-04-04 09:53:28 -0300

    update

--- connect.js
+++ connect.js
@@ -2,8 +2,8 @@ const { Pool } = require("pg").Pool;
 require("dotenv").config();
 
 const pool = new Pool({
-  user: process.env.Username,
-  password: process.env.Password,
+  user: process.env.USERNAME,
+  password: process.env.PASSWORD,
   host: "localhost",
   port: 5432,
   database: process.env.DB,
