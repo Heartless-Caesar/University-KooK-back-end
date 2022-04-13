ba3735a867e3cb471121c2ba5955e7cf42296970
caesar <joaovictorcsatu@gmail.com>
2022-04-13 00:02:14 -0300

    Update

--- controllers/recepieControllers.js
+++ controllers/recepieControllers.js
@@ -1,6 +1,15 @@
 const recepies = require("../models/recepies");
+const multer = require("multer");
 const fs = require("fs");
 
 const createRecepie = async () => {
-  //GET REQ.BODY AND UPLOAD IMAGE FOR CONVERSION
+  const Storage = multer.diskStorage({
+    destination: "uploads",
+    filename: (req, file, cb) => {
+      cb(null, file.originalname);
+    },
+  });
+  const upload = multer({ storage: Storage }).single("testimage");
 };
+
+module.exports = { createRecepie };
