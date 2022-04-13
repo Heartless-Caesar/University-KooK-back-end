e7aa11728170f3ef0235468634ec5d15113dde67
caesar <joaovictorcsatu@gmail.com>
2022-04-13 00:01:59 -0300

    Added basic fiel upload

--- routes/routes.js
+++ routes/routes.js
@@ -5,6 +5,4 @@ const { login, registerUser } = require("../controllers/controllers");
 router.route("/auth/register").post(registerUser);
 router.route("/auth/login").post(login);
 
-//router.route("/").get(getAll);
-
 module.exports = router;
