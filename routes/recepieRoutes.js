91c2eb6e5f0d3183527b69c63fd04c79b17503c3
caesar <joaovictorcsatu@gmail.com>
2022-04-14 01:18:19 -0300

    Update

--- routes/recepieRoutes.js
+++ routes/recepieRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { createRecepie, upload } = require("../controllers/recepieControllers");
 
-router.route("/recepie/create").post([createRecepie, upload]);
+router.route("/recepie/create").post(createRecepie, upload);
 //router.route("/:id").post(login);
 
 module.exports = router;
