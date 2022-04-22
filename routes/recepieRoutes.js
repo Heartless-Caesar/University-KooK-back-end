6e88d7d5c84da8fb43cb4952102a0684382d562d
caesar <joaovictorcsatu@gmail.com>
2022-04-22 08:10:08 -0300

    Implemented deletion functionality

--- routes/recepieRoutes.js
+++ routes/recepieRoutes.js
@@ -6,11 +6,13 @@ const {
   getRecepie,
   getAllRecepies,
   updateRecepie,
+  deleteRecepie,
 } = require("../controllers/recepieControllers");
 
 router.route("/recepie/create").post(upload, createRecepie);
-router.route("/recepie/:_id").get(getRecepie);
 router.route("/recepie/:_id").put(upload, updateRecepie);
+router.route("/recepie/:_id").delete(deleteRecepie);
+router.route("/recepie/:_id").get(getRecepie);
 router.route("/recepies").get(getAllRecepies);
 
 module.exports = router;
