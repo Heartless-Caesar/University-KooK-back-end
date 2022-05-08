b4b538d52b85c177af3452a51ce593c595fcaa14
caesar <joaovictorcsatu@gmail.com>
2022-05-08 20:53:46 -0300

    Created comment post controller

---
+++ controllers/rating_controllers/comment_POST.js
@@ -0,0 +1,21 @@
+const { comentarios, avaliacoes } = require("../../models");
+const { StatusCodes } = require("http-status-codes");
+
+const postComment = async (req, res) => {
+  const { _id } = req.params;
+  const { descricao } = req.body;
+
+  const checkRating = await avaliacoes.findOne({ where: { id: _id } });
+
+  if (!checkRating) {
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ msg: `No rating associated to the id ${_id}` });
+  }
+
+  const newComment = await comentarios.create({ descricao });
+
+  res
+    .status(StatusCodes.OK)
+    .json({ msg: `Comment created`, comment: newComment });
+};
--- models/comentarios.js
+++ models/comentarios.js
@@ -28,6 +28,7 @@ module.exports = (sequelize, DataTypes) => {
     },
     {
       sequelize,
+      tableName: "comentarios",
       modelName: "comentarios",
     }
   );
